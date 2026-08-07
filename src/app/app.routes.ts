b113e32a8d73da115c4506041b3d3c32932529c7
ThomasPeruch <peruchthomas@gmail.com>
2026-08-07 10:36:03 -0300

    toolbar routing templates; find component and insert component created; change app theme

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { InsertComponent } from './insert/insert.component'
+import { FindComponent } from './find/find.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'cadastro', component: InsertComponent },
+    { path: 'consulta', component: FindComponent }
+];
