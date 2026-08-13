2a182868f92c2e8bdade0224df4891667c2b5c68
ThomasPeruch <peruchthomas@gmail.com>
2026-08-13 10:36:39 -0300

    model created; getting data from form to model

---
+++ src/app/insert/client.ts
@@ -0,0 +1,15 @@
+import { v4 as uuid } from 'uuid';
+
+export class Client {
+    id?: string;
+    name?: string;
+    cpf?: string;
+    birthDate?: string;
+    email?: string;
+
+    static newClient(){
+        const client = new Client();
+        client.id = uuid();
+        return client;
+    }
+}
--- src/app/insert/insert.component.ts
+++ src/app/insert/insert.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatCardModule } from '@angular/material/card'
+import { MatFormField, MatLabel } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { FormsModule } from '@angular/forms'
+import { MatIcon } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { Client } from "./client";
 
 @Component({
   selector: 'app-insert',
-  imports: [],
+  imports: [
+    FlexLayoutModule, 
+    MatCardModule, 
+    FormsModule, 
+    MatFormField, 
+    MatLabel, 
+    MatInputModule, 
+    MatIcon,
+    MatButtonModule
+  ],
   templateUrl: './insert.component.html',
   styleUrl: './insert.component.scss'
 })
 export class InsertComponent {
+  client: Client = Client.newClient();
 
+  logObject(){
+    console.log(this.client)
+  }
 }
